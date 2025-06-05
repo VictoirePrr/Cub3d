7e8665345d826126a6f5eb701b26769cc17f962e
Pierrick Naessens <pierrick.naessens62@gmail.com>
2025-06-05 14:20:11 +0200

    test code with lovecode

--- src/cam/render.c
+++ src/cam/render.c
@@ -9,7 +9,11 @@ typedef struct s_ray
 	int			map_y;
 	double delta_dist_x; // Distance  intersections X
 	double delta_dist_y; // Distance  intersections Y
-	int hit;             // check wall
+	double side_dist_x;  // Distance jusqu'à prochaine intersection X
+	double side_dist_y;  // Distance jusqu'à prochaine intersection Y
+	int step_x;          // Direction du pas (-1 ou 1)
+	int			step_y;
+	int hit; // check wall
 }				t_ray;
 
 void	put_pixel(t_cub3d *cub3d, int x, int y, int color)
@@ -36,6 +40,32 @@ void	init_ray(t_ray *ray, t_camera *cam, int x)
 	ray->delta_dist_x = fabs(1 / ray->ray_dir_x);
 	ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
 	ray->hit = 0;
+}
+
+void	calculate_step_and_side_dist(t_ray *ray)
+{
+	if (ray->ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (ray->cam->pos_x - ray->map_x) * ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - ray->cam->pos_x)
+			* ray->delta_dist_x;
+	}
+	if (ray->ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (ray->cam->pos_y - ray->map_y) * ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - ray->cam->pos_y)
+			* ray->delta_dist_y;
+	}
 }
 
 void	debug_init_ray(t_ray *ray, int x)
